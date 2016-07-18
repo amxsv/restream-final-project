fb0ebb0f1f4940ae8cb7b1af10edb6738bf2d515
Anton Chepenko <chepenkoanton2011@yandex.ru>
2016-07-19 00:30:51 +0300

    add role column, managers

--- app/controllers/web/admin/users_controller.rb
+++ app/controllers/web/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Web::Admin::UsersController < Web::Admin::ApplicationController
   def index
     @per_page = params[:per_page] || Kaminari.config.default_per_page
-    @users = User.workers.page(params[:page]).per(@per_page)
+    @users = User.all.page(params[:page]).per(@per_page)
 
     @tasks_count = User.tasks_count
   end
